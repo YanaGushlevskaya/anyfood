12f8eb4b8991c49529d380c9cbb4b4d0e35bf426
Yana Gushlevskaya <jasynja@ukr.net>
2021-06-01 09:23:49 +0300

    change package json

--- simple_api/server.js
+++ simple_api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const api = require('./api');
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3001;
 
 const app = express();
 
